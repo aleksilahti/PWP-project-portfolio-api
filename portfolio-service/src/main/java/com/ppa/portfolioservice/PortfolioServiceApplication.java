d0147687d1beea88e8af4133d2175176b900a907
aleksilahti <aleksi.mikael.lahti@gmail.com>
2024-01-30 22:42:21 +0200

    Get backend up and running. Create initial project class and serve it via the Spring web API

--- portfolio-service/src/main/java/com/ppa/portfolioservice/PortfolioServiceApplication.java
+++ portfolio-service/src/main/java/com/ppa/portfolioservice/PortfolioServiceApplication.java
@@ -1,13 +1,42 @@
 package com.ppa.portfolioservice;
 
+import java.time.Instant;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.ppa.portfolioservice.project.Project;
+
 
 @SpringBootApplication
+@RestController
 public class PortfolioServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioServiceApplication.class, args);
+	}
+
+	@GetMapping
+	public List <Project> hello() {
+
+		return List.of(
+			new Project(
+				1L,
+				2L,
+				"Placeholder Project",
+				"ARCHIVED",
+				"ShortDesc",
+				"Normal description is here",
+				List.of("url1", "url2"),
+				List.of("id", "id2"),
+				List.of("tag1", "tag2"),
+				Date.from(Instant.now()),
+				Date.from(Instant.now())
+			)
+		);
 	}
 
 }
